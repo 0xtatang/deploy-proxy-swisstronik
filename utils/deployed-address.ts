0bb2b18f87a5f8dcc73006ea1070ab3ec65270e1
panenke2 <panenke2@gmail.com>
2024-07-30 14:28:46 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xF26235a0E0536B22043AF13c18AFb89aed4AFdDA'
+export const ProxyAdmin = '0x3FEFb818e38a4766740f3f8BEe9ecCEC4af29E0a'
+export const SWTRImplementation = '0xDeF2C7F09Fd2547367DD7ec7a6972e0474Be56D0'
     
